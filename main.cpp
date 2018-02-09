66d930e870196c5e9b354f490ed4fde25ef81854
Peter Fischer <peter.fischer.geo@web.de>
2018-02-09 16:25:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "cpl_string.h"
 #include "ogrsf_frmts.h"
 #include <ogr_geometry.h>
+#include "gdal_alg.h"
 
 
 
@@ -249,6 +250,15 @@ void clipDB(const std::string& AbsPathDB, const std::string& tmpPath_Rectangle,
 
 void bufferClip(const std::string& tmpPath_clip, const std::string& tmpPath_buffer){
 
+    /* function which buffers a given shp-file holding LineStrings
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
     GDALDataset * poDS1;
     poDS1 = (GDALDataset*) GDALOpenEx( tmpPath_clip.c_str(), GDAL_OF_VECTOR, NULL, NULL, NULL );
 
@@ -278,7 +288,7 @@ void bufferClip(const std::string& tmpPath_clip, const std::string& tmpPath_buff
     }
 
     OGRLayer *poLayer;
-    poLayer = poDS->CreateLayer( "point_out", poDBLayer->GetSpatialRef(), wkbLineString, NULL );
+    poLayer = poDS->CreateLayer( "point_out", poDBLayer->GetSpatialRef(), wkbPolygon, NULL );
     if( poLayer == NULL ){
         printf( "Layer creation failed.\n" );
         exit( 1 );
@@ -300,14 +310,19 @@ void bufferClip(const std::string& tmpPath_clip, const std::string& tmpPath_buff
     while( (poFeature = poDBLayer->GetNextFeature()) != NULL )
     {
 
-        OGRFeatureDefn * poFDefn = poDBLayer->GetLayerDefn();
-
         OGRGeometry * poGeometry;
         poGeometry = poFeature->GetGeometryRef();
+        const char * pszFName = "fclass";
+        std::string currentFeatureClass = poFeature->GetFieldAsString(pszFName);
+        std::string motorway = "motorway";
+        if(currentFeatureClass.compare(motorway)){
+            std::cout << "motorway" << std::endl;
+        }
+
         OGRLineString * poLineString;
         poLineString = (OGRLineString *) poGeometry;
         double Angle = 0.;
-        for(unsigned int i = 0; i < poLineString->getNumPoints() - 1; i++){
+        for(int i = 0; i < poLineString->getNumPoints() - 1; i++){
             OGRPoint MyPoint1, MyPoint2;
             poLineString->getPoint(i, &MyPoint1);
             poLineString->getPoint(i + 1, &MyPoint2);
@@ -343,9 +358,77 @@ void bufferClip(const std::string& tmpPath_clip, const std::string& tmpPath_buff
     }
 
     GDALClose( poDS );
+
 }
 
-void rasterizeShp(const std::string& tmpPath_buffer, const std::string& tmpMaskImage){
+void rasterizeShp(const std::string& tmpPath_bufferImg, const std::string& tmpPath_bufferVec, const std::string& tmpMaskImage){
+
+    /* function to rasterize a given shape file
+     * as input a shape file with polygons is expected, where each polygon holds an attribute "Angle" as floating point number
+     * the attribute value "Angle" is then burned into the output tiff image of type float32 which shares the Geodetic Datum and
+     * GSD  of the given input image.
+     *
+     *
+     *
+     * */
+
+    //* * * * * * * * * * * get spatial extent and geoinfo of mask image * * * * * * * * * * *
+    GDALDataset  *poDataset;
+    poDataset = (GDALDataset *) GDALOpen( tmpMaskImage.c_str(), GA_ReadOnly );
+    double adfGeoTransform[6];
+    poDataset->GetGeoTransform( adfGeoTransform );
+
+    //* * * * * * * * * * * create output file for rasterized buffer image * * * * * * * * * * *
+    const char *pszFormat = "GTiff";
+    GDALDriver *poDriver;
+    char **papszMetadata;
+    poDriver = GetGDALDriverManager()->GetDriverByName(pszFormat);
+    papszMetadata = poDriver->GetMetadata();
+
+    GDALDataset *poDstDS;
+    char **papszOptions = NULL;
+    poDstDS = poDriver->Create( tmpPath_bufferImg.c_str(), poDataset->GetRasterXSize(), poDataset->GetRasterYSize(), 1, GDT_Float32,
+                                papszOptions );
+    poDstDS->SetGeoTransform( adfGeoTransform );
+    poDstDS->SetProjection(poDataset->GetProjectionRef());
+
+    GDALRasterBand *poBand;
+    float * ImgData;
+    ImgData = (float *) CPLMalloc(sizeof(float) * poDataset->GetRasterXSize() * poDataset->GetRasterYSize());
+
+    for (int i = 0; i < poDataset->GetRasterXSize() * poDataset->GetRasterYSize(); i++){
+        ImgData[i] = 0.0;
+    }
+
+    poBand = poDstDS->GetRasterBand(1);
+    poBand->RasterIO( GF_Write, 0, 0, poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
+                      ImgData, poDataset->GetRasterXSize(), poDataset->GetRasterYSize(), GDT_Float32, 0, 0 );
+
+    /* close Dataset from which we got geoinfo and raster size */
+    GDALClose( (GDALDatasetH) poDataset );
+
+    //* * * * * * * * * * * get geometries to rasterize from vector file * * * * * * * * * * *
+
+    GDALDataset * poDS1;
+    poDS1 = (GDALDataset*) GDALOpenEx( tmpPath_bufferVec.c_str(), GDAL_OF_VECTOR, NULL, NULL, NULL );
+
+    if( poDS1 == NULL ){
+        printf( "Open of clipped file failed.\n" );
+        exit( 1 );
+    }
+    OGRLayer * poDBLayer;
+    poDBLayer = poDS1->GetLayer(0);
+
+    char** options = nullptr;
+
+    //options = CSLSetNameValue(options, "ALL_TOUCHED", "TRUE");
+    options = CSLSetNameValue(options, "ATTRIBUTE", "Angle");
+
+    int panBandList = 1;
+    GDALRasterizeLayers(poDstDS, 1, &panBandList , 1, (OGRLayerH*)&poDBLayer,  NULL, NULL, NULL, options, NULL, NULL);
+
+    GDALClose( (GDALDatasetH) poDBLayer );
+    GDALClose( (GDALDatasetH) poDstDS );
 
 }
 
@@ -354,32 +437,30 @@ int main(int argc, char *argv[])
     OGRRegisterAll();
     GDALAllRegister();
 
-    const std::string AbsPathImage =      "/home/peter/Desktop/data/img/ON1678_clipped.tif";
-    const std::string AbsPathDB =         "/home/peter/Desktop/data/GermanyRoads.sqlite";
-    const std::string tmpPath_Rectangle = "/home/peter/Desktop/data/vec/rectangle.shp";
-    const std::string tmpPath_clip =      "/home/peter/Desktop/data/vec/clip.shp";
-    const std::string tmpPath_buffer =    "/home/peter/Desktop/data/vec/buffer.shp";
+    const std::string AbsPathImage =      "/home/fisc_p0/Desktop/data/img/ON1678_clipped.tif";
+    const std::string AbsPathDB =         "/home/fisc_p0/Desktop/data/GermanyRoads.sqlite";
+    const std::string tmpPath_Rectangle = "/home/fisc_p0/Desktop/data/vec/rectangle.shp";
+    const std::string tmpPath_clip =      "/home/fisc_p0/Desktop/data/vec/clip.shp";
+    const std::string tmpPath_buffer =    "/home/fisc_p0/Desktop/data/vec/buffer.shp";
+    const std::string AbsPathBufferImage =      "/home/fisc_p0/Desktop/data/img/ON1678buffer.tif";
 
-/*
+    /*
     OGRLinearRing MyRing;
     OGRSpatialReference SRS;
     SRS.SetWellKnownGeogCS("WGS84");
     MyRing.assignSpatialReference(&SRS);
-
     createBoundingBox(AbsPathImage, MyRing);
-
     OGRPolygon MyPolygon;
     MyPolygon.assignSpatialReference(&SRS);
     MyPolygon.addRing(&MyRing);
-
     createTmpLayer(MyPolygon, tmpPath_Rectangle);
-
     clipDB(AbsPathDB, tmpPath_Rectangle, tmpPath_clip);
-*/
 
+    */
     bufferClip(tmpPath_clip, tmpPath_buffer);
 
 
+    rasterizeShp(AbsPathBufferImage, tmpPath_buffer, AbsPathImage);
     //loadCompleteOSMToMem(MyPolygon);
 
 
